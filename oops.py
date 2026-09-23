3bded57f931467bbdbaaddeae42cc54e9129cf6e
Riya <iamriyasaryam@gmail.com>
2026-09-23 22:50:48 +0530

    Encapsulation

--- oops.py
+++ oops.py
@@ -24,24 +24,49 @@
 
 
 
-class Account:
-    def __init__(self, acc_holder, balance):
-        self.acc_holder = acc_holder
-        self.balance = balance
+# class Account:
+#     def __init__(self, acc_holder, balance):
+#         self.acc_holder = acc_holder
+#         self.balance = balance
 
-    def deposit(self, amount):
-        self.balance += amount
-        return self.balance
+#     def deposit(self, amount):
+#         self.balance += amount
+#         return self.balance
     
-    def withdraw(self, amount):
-        self.balance -= amount
-        return self.balance
+#     def withdraw(self, amount):
+#         self.balance -= amount
+#         return self.balance
 
-U1 = Account("Riya", 500)
+# U1 = Account("Riya", 500)
 
-U1.withdraw(50)
+# U1.withdraw(50)
 
-print(U1.balance)
+# print(U1.balance)
 
 
+class Car:
+    def __init__(self, brand, model):
+        self.__brand =  brand
+        self.model = model
 
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
+    # Getters / Encapusaltion
+    def get_brand(self):
+        return self.__brand + "!"
+
+
+# class ElectricCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+car = ElectricCar("Audi", "A6", 500)
+
+print(car.get_brand())
